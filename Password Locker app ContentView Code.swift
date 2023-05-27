08aab9c6dc0ec07e8deba6b982a5a97c4545f893
Dylan Labrum <115735002+DylanLabrum@users.noreply.github.com>
2023-05-27 14:23:35 -0600

    Add files via upload

---
+++ Password Locker app ContentView Code.swift
@@ -0,0 +1,13 @@
+1  import SwiftUI
+2
+3  struct ContentView: View {
+4      var body: some View {
+5          VStack {
+6              Image(systemName: "globe")
+7                  .imageScale(.large)               
+8                  .foregroundColor(.accentColor)
+9              Text("Hello, world!")
+10           }
+11      }
+12  }
+13 
