4b75dc0dcfeca1decc151c2a067eafdb2f1790d2
Dylan Labrum <115735002+DylanLabrum@users.noreply.github.com>
2023-05-27 14:23:00 -0600

    Add files via upload

---
+++ PasswordLockerapp_TemplateApp.swift
@@ -0,0 +1,17 @@
+//
+//  PasswordLockerapp.swift
+//  PasswordLockerapp
+//
+//  Created by Dylan Labrum on 5/27/23.
+//
+
+import SwiftUI
+
+@main
+struct PasswordLocker_TemplateApp: passwordLocker {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
